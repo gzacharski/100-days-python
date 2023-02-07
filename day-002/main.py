d168509011b998ee929ee98a8e3fdef9999eae93
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-02-07 21:29:14 +0100

    feat: add main.py

---
+++ day-002/main.py
@@ -0,0 +1,29 @@
+# Data types
+
+# String
+
+print("Hello"[4])
+
+print("123" + "345")
+
+# Integer
+print(123 + 345)
+print(123_342_432_342)
+
+# Float
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
+
+num_char = len(input("What is your name?"))
+
+print(type(num_char))
+
+print("Your name has " + str(num_char) + " characters.")
+
+a = 123
+print(type(a))
+print(type(str(a)))
+print(type(float(a)))
