872b3592d6e2fa2d50c83b9c6fa76e02f537ce68
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-02-11 23:01:59 +0100

    feat: update main.py

--- day-004/main.py
+++ day-004/main.py
@@ -10,3 +10,12 @@ print(random_float)
 
 love_score = random.randint(1, 100)
 print(f"Your love score is {love_score}")
+
+state1 = "Delaware"
+state2 = "Pensylwani"
+
+states_of_america = ["Delaware", "Pennsylvania"]
+states_of_america.append("Angeland")
+print(states_of_america)
+print(states_of_america + ["dasd","21"])
+print(states_of_america.extend(["dasd", "dawws"]))
