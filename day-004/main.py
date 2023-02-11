e24ddb1e94d29a9fca04e3a53fda7a1bc0dda76d
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-02-11 22:14:57 +0100

    feat: add my_module.py

--- day-004/main.py
+++ day-004/main.py
@@ -1,4 +1,12 @@
 import random
+import my_module
 
 random_integer = random.randint(100, 200)
 print(random_integer)
+print(my_module.pi)
+
+random_float = random.random() * 5
+print(random_float)
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")
