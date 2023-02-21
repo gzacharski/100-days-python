8cbe7f516db6b105a8c45480a19d9c82d724432a
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-02-21 23:01:45 +0100

    feat: add main day 8

---
+++ day-008/main.py
@@ -0,0 +1,15 @@
+def greet():
+    print("hello")
+    print("How do you do?")
+    print("Isn't the weather nice today?")
+
+
+greet()
+
+
+def greet_with_name(name=""):
+    print(f"hello {name}")
+    print(f"How do you do {name}?")
+
+
+greet_with_name("Grzegorz")
