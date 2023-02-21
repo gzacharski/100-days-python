074d10e1106f2434fcd778935af273aff81f3884
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-02-21 23:07:48 +0100

    feat: add main day 8

--- day-008/main.py
+++ day-008/main.py
@@ -13,3 +13,12 @@ def greet_with_name(name=""):
 
 
 greet_with_name("Grzegorz")
+
+
+def greet_with(name="", location=""):
+    print(f"hello {name}")
+    print(f"What is it like in {location}?")
+
+
+greet_with("Grzegorz", "Warsaw")
+greet_with(location="Warsaw", name="Grzegorz")
