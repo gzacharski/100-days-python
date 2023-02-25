a548de0b6b1fbfb0d2076b8c9de3cd5508ee0922
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-02-25 16:04:49 +0100

    feat: add docstrings

--- day-010/main.py
+++ day-010/main.py
@@ -7,6 +7,10 @@ print(output)
 
 
 def format_name(first_name: str = "", last_name: str = ""):
+    """
+    Take a first and last name and format it to return
+    the title case version of the name.
+    """
     first_name_formatted = first_name.strip().lower().title()
     last_name_formatted = last_name.strip().lower().title()
     return f"{first_name_formatted} {last_name_formatted}"
