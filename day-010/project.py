594e5c55848ef05f809e75538ff3223d2c38176c
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-02-25 16:09:23 +0100

    feat: add functions to project

--- day-010/project.py
+++ day-010/project.py
@@ -1,3 +1,28 @@
 from art import logo
 
 print(logo)
+
+
+# Calculator
+
+# add
+def add(a: int, b: int):
+    return a + b
+
+
+# subtract
+def subtract(a: int, b: int):
+    return a - b
+
+
+# multiply
+def multiply(a: int, b: int):
+    return a * b
+
+
+# divide
+def divide(a: int, b: int):
+    if b == 0:
+        print("You cant divide by zero!")
+        return
+    return a / b
