4854fc65654cc3c5f492476ff4dfd6a617175a50
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-02-25 15:42:56 +0100

    feat: add day 10

--- day-010/main.py
+++ day-010/main.py
@@ -6,10 +6,10 @@ output = my_function()
 print(output)
 
 
-def format_name(first_name: str, last_name: str):
+def format_name(first_name: str = "", last_name: str = ""):
     first_name_formatted = first_name.strip().lower().title()
     last_name_formatted = last_name.strip().lower().title()
     return f"{first_name_formatted} {last_name_formatted}"
 
 
-print(format_name("GrzEgorz", " zacharski  "))
+print(format_name("GrzegorZ"))
