aff3a089635074b77024261020f17b943ce0dac4
Grzegorz Zacharski <61288023+gzacharski@users.noreply.github.com>
2023-02-26 16:16:26 +0100

    feat: add project day 10

--- day-010/project.py
+++ day-010/project.py
@@ -26,3 +26,25 @@ def divide(a: int, b: int):
         print("You cant divide by zero!")
         return
     return a / b
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+number_1 = int(input("What is the first number? "))
+number_2 = int(input("What is the second number? "))
+for symbol in operations.keys():
+    print(f"Type {symbol} for {operations[symbol].__name__}.")
+
+operation = str(input("What operation do you want to do? "))
+if not operation in operations:
+    print(f"There is no such an operation like: {operation}.")
+    exit()
+
+func = operations[operation]
+result = func(number_1, number_2)
+print(f"{number_1} {operation} {number_2} = {result}")
