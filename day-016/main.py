095e6c3e65abc86e5a3b8c73ddbb9f785127505e
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-03-04 12:25:17 +0100

    feat: add final project from day 16

--- day-016/main.py
+++ day-016/main.py
@@ -1,18 +1,22 @@
-# from turtle import Turtle, Screen
-from prettytable import PrettyTable
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape("turtle")
-# timmy.color("coral")
-# timmy.forward(100)
-#
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
+menu = Menu()
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
 
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
-table.align = "l"
-print(table)
+while True:
+    choice = input(f"What would you like? ({menu.get_items()}): ").lower()
+    if choice == "off":
+        print("Goodbye")
+        break
+    elif choice == "report":
+        money_machine.report()
+        coffee_maker.report()
+    elif choice in menu.get_items():
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+    else:
+        print("error")
