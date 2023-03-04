f635e7a72511eb5cbc8de933a443cdf8d54f63ea
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-03-04 13:28:41 +0100

    feat: add init main day 17

---
+++ day-017/main.py
@@ -0,0 +1,31 @@
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+
+class Car:
+    def __init__(self):
+        self.seats = 5
+
+    def enter_race_mode(self):
+        self.seats = 2
+
+
+user_1 = User("001", "Grzegorz")
+user_2 = User("002", "Anna")
+print(user_1)
+print(user_1.username)
+
+user_1.follow(user_2)
+
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
