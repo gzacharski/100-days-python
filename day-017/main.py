69310ccb5013ce28aaedffcf2b9f5710dba32f3c
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-03-04 15:54:50 +0100

    feat: add final project of day 17

--- day-017/main.py
+++ day-017/main.py
@@ -1,31 +1,16 @@
-class User:
-    def __init__(self, user_id, username):
-        self.id = user_id
-        self.username = username
-        self.followers = 0
-        self.following = 0
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
 
-    def follow(self, user):
-        user.followers += 1
-        self.following += 1
+question_bank = []
+for question in question_data:
+    question_text = question.get('text')
+    question_answer = question.get('answer')
+    question_bank.append(Question(question_text, question_answer))
 
+quiz_brain = QuizBrain(question_bank)
+while quiz_brain.still_has_questions():
+    quiz_brain.next_question()
 
-class Car:
-    def __init__(self):
-        self.seats = 5
-
-    def enter_race_mode(self):
-        self.seats = 2
-
-
-user_1 = User("001", "Grzegorz")
-user_2 = User("002", "Anna")
-print(user_1)
-print(user_1.username)
-
-user_1.follow(user_2)
-
-print(user_1.followers)
-print(user_1.following)
-print(user_2.followers)
-print(user_2.following)
+print("You've completed the quiz")
+print(f"Your final score was: {quiz_brain.score}/{quiz_brain.question_number}.")
