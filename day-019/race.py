83d159a899fd5aedd3546add7a7ef3efe3088fe5
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-03-05 21:44:15 +0100

    feat: add race.py

---
+++ day-019/race.py
@@ -0,0 +1,35 @@
+from turtle import Turtle, Screen
+import random
+
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win? Enter the color:")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+all_turtle = []
+
+y = -140
+for color in colors:
+    tim = Turtle(shape="turtle")
+    tim.color(color)
+    tim.penup()
+    y += 40
+    tim.goto(x=-230, y=y)
+    all_turtle.append(tim)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtle:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
+screen.exitonclick()
