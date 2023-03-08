dbc6dc32ac8235da0603c55f4900295f95743a97
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-03-08 22:25:56 +0100

    feat: add scoreboard.py

--- day-022/ball.py
+++ day-022/ball.py
@@ -9,13 +9,25 @@ class Ball(Turtle):
         self.dot(size=20)
         self.penup()
         self.color("white")
-        self.x_move = 1
-        self.y_move = 1
+        self.x_move = 10
+        self.y_move = 10
+        self.speed_factor = 0.1
 
     def move(self):
-        x = self.xcor() + self.x_move
-        y = self.ycor() + self.y_move
+        x = self.xcor() + self.x_move * self.speed_factor
+        y = self.ycor() + self.y_move * self.speed_factor
         self.goto(x, y)
 
-    def bounce(self):
+    def bounce_y(self):
         self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def reset(self):
+        self.goto(0, 0)
+        self.x_move *= -1
+        self.speed_factor = 0.1
+
+    def increase_speed(self):
+        self.speed_factor += 0.05
--- day-022/main.py
+++ day-022/main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -13,6 +14,7 @@ paddle_right = Paddle(xcor=350, ycor=0)
 paddle_left = Paddle(xcor=-350, ycor=0)
 
 the_ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkeypress(paddle_right.move_up, "Up")
@@ -27,7 +29,22 @@ while game_is_on:
     screen.update()
     the_ball.move()
 
+    # Detect collision with wall
     if the_ball.ycor() > 290 or the_ball.ycor() < -290:
-        the_ball.bounce()
+        the_ball.bounce_y()
+
+    # Detect collision with r_paddle
+    if the_ball.distance(paddle_right) < 50 and the_ball.xcor() > 330 or the_ball.distance(
+            paddle_left) < 50 and the_ball.xcor() < -330:
+        the_ball.bounce_x()
+        the_ball.increase_speed()
+
+    if the_ball.xcor() < -380:
+        the_ball.reset()
+        scoreboard.score_right()
+
+    if 380 < the_ball.xcor():
+        the_ball.reset()
+        scoreboard.score_left()
 
 screen.exitonclick()
