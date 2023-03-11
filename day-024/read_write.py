4284e4467c4d2fa91fe327e8c0d2b36b11bca2dd
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-03-11 14:45:43 +0100

    feat: add file support

--- day-024/read_write.py
+++ day-024/read_write.py
@@ -1,11 +1,10 @@
-with open("my_file.txt") as file:
+with open("D:\\udemy\\python\\src\\day-024\\my_file.txt") as file:
     contents = file.read()
     print(contents)
 
 with open("my_file.txt", mode="a") as file:
     file.write("New text.")
     print(contents)
-
 
 with open("my_new_file.txt", mode="w") as file:
     file.write("New text.")
