a80af26da89e6946f42ec2a101a8fa44c5bbee91
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-03-15 21:44:51 +0100

    feat: create a dataframe from scratch

--- day-025/main.py
+++ day-025/main.py
@@ -40,3 +40,17 @@ print(data.temp.max())
 print(data[data.day == "Monday"])
 print(data[data.temp == data.temp.max()])
 print(data[data.temp == 14])
+
+monday = data[data.day == "Monday"]
+monday_temp = int(monday.temp)
+monday_temp_F = monday_temp * 9 / 5 + 32
+print(monday_temp_F)
+
+# Create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
