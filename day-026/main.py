5fd6c97bb6a9d2429d0d58dee2c9c5bd45f496c3
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-03-18 17:08:32 +0100

    feat: add day 26

---
+++ day-026/main.py
@@ -0,0 +1,10 @@
+numbers = [1, 2, 3]
+new_list = []
+for n in numbers:
+    add_1 = n + 1
+    new_list.append(add_1)
+
+comprehension_list = [n + 1 for n in numbers]
+
+print(new_list)
+print(comprehension_list)
