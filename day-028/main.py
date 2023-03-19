6e13252b726f1d86695c4021f4cd6b0c4ee14d69
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-03-19 14:28:25 +0100

    feat: add setup of UI

--- day-028/main.py
+++ day-028/main.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Canvas, PhotoImage
+from tkinter import Tk, Canvas, PhotoImage, Label, Button
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -21,11 +21,23 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+title_label.grid(column=1, row=0)
+
 photo_image = PhotoImage(file="tomato.png")
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(103, 112, image=photo_image)
 canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15, "bold"))
+check_marks.grid(column=1, row=3)
 
 window.mainloop()
