ae3045de4ca78741fbb14da4c5d74793044c16ac
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-03-21 21:08:10 +0100

    feat: add init UI

--- day-029/main.py
+++ day-029/main.py
@@ -3,3 +3,16 @@
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+from tkinter import Canvas, PhotoImage, Tk
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+photo_image = PhotoImage(file="logo.png")
+
+canvas = Canvas(width=200, height=200)
+canvas.create_image(100, 100, image=photo_image)
+canvas.pack()
+
+window.mainloop()
