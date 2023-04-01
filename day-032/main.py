f44cb1d509546f5c78ef0a9c3da9dd0ca3718f18
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-04-02 00:36:43 +0200

    feat: add sending email

--- day-032/main.py
+++ day-032/main.py
@@ -4,7 +4,8 @@ my_email = ""
 password = ""
 recipient_email = ""
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs=recipient_email)
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email, to_addrs=recipient_email,
+                        msg="Subject:Hello\n\nThis is the body of my email.")
