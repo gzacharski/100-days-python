6a852dbc5a3ba7979c5c0b50d94839e0d6b71ade
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-04-02 17:53:51 +0200

    feat: challenge 1 - send random quote

--- day-032/main.py
+++ day-032/main.py
@@ -1,11 +1,22 @@
 import smtplib
+import datetime as dt
+import random
 
 my_email = ""
 password = ""
 recipient_email = ""
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email, to_addrs=recipient_email,
-                        msg="Subject:Hello\n\nThis is the body of my email.")
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 6:
+    with open("quotes.txt", "r") as quotes:
+        lines = quotes.readlines()
+
+    chosen_line = random.choice(lines)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs=recipient_email,
+                            msg=f"Subject:Monday Motivation\n\n{chosen_line}")
+else:
+    print("No email sent. Wait till Monday")
