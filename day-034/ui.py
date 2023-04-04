554849453e2e3dfaf0f97920bea2a177d3ab8d78
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-04-04 22:54:47 +0200

    feat: add UI

--- day-034/ui.py
+++ day-034/ui.py
@@ -1,2 +1,28 @@
-THEME_COLOR = "#375362"
+from tkinter import *
 
+THEME_COLOR = "#375362"
+FONT = ("Arial", 20, "italic")
+
+
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.configure(background=THEME_COLOR, padx=20, pady=20)
+
+        self.score_label = Label(text="Score: 0", foreground="white", background=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, background="white")
+        self.question_text = self.canvas.create_text(150, 125, text="Some question text", font=FONT, fill=THEME_COLOR)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(column=0, row=2)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(column=1, row=2)
+
+        self.window.mainloop()
