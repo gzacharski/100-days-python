4513bd32ae3815f7dd0aa678863cac1b8456b2bc
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-04-13 23:35:01 +0200

    feat: add final script

--- day-035/main.py
+++ day-035/main.py
@@ -1,15 +1,16 @@
 import requests
 import json
+from twilio.rest import Client
+import os
 
-API_KEY = ""
-
-# OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather"
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
+API_KEY = os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
+
 params = {
-    # "lat": 20.6275,
-    "lat": 55.864239,
-    # "lon": 50.8703,
-    "lon": -4.251806,
+    "lat": 45.326908,
+    "lon": 14.441000,
     "appid": API_KEY,
     "exclude": "current,minutely,daily"
 }
@@ -21,4 +22,7 @@ weather_data = json.dumps(response.json(), indent=2)
 weather_response = response.json()
 weather_list = [weather for weather in weather_response.get("list")[:12] if weather.get("weather")[0].get("id") < 700]
 if len(weather_list) > 1:
-    print("Bring an Umbrella")
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(body="It's going to rain today. Remember to bring umbrella", from_="+15074146919",
+                                     to="+48665762682")
+    print(message.status)
