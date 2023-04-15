2ccec274f8f1763e948a4da03482e5cd4551a876
Grzegorz Zacharski <g.zacharski@wp.pl>
2023-04-15 17:31:52 +0200

    feat: add solution for step 2 and 3

--- day-036/main.py
+++ day-036/main.py
@@ -1,6 +1,8 @@
+import random
 import requests
 from datetime import datetime as dt
 import os
+from twilio.rest import Client as TwilioClient
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -8,9 +10,27 @@ COMPANY_NAME = "Tesla Inc"
 # STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
-API_KEY = os.environ.get("API_KEY")
+#  Run before
+#  export TWILIO_ACCOUNT_SID=<change_me>;
+#  export TWILIO_AUTH_TOKEN=<change_me>;
+#  export TWILIO_FROM_NUMBER=<change_me>;
+#  export TWILIO_TO_NUMBER=<change_me>;
+#  export STOCK_API_KEY=<change_me>;
+#  export NEWS_API_KEY=<change_me>;
+
+
+TWILIO_ACCOUNT_SID = os.environ["TWILIO_ACCOUNT_SID"]
+TWILIO_AUTH_TOKEN = os.environ["TWILIO_AUTH_TOKEN"]
+TWILIO_FROM_NUMBER = os.environ["TWILIO_FROM_NUMBER"]
+TWILIO_TO_NUMBER = os.environ["TWILIO_TO_NUMBER"]
+
+STOCK_API_KEY = os.environ.get("STOCK_API_KEY")
 STOCK_URL = "https://www.alphavantage.co/query"
-params = {"function": "TIME_SERIES_DAILY_ADJUSTED", "symbol": STOCK, "interval": "5min", "apikey": API_KEY}
+STOCK_PARAMS = {"function": "TIME_SERIES_DAILY_ADJUSTED", "symbol": STOCK, "interval": "5min", "apikey": STOCK_API_KEY}
+
+NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
+NEWS_URL = "https://newsapi.org/v2/everything"
+NEWS_PARAMS = {"qInTitle": COMPANY_NAME, "apiKey": NEWS_API_KEY}
 
 
 class DailyStockData:
@@ -33,32 +53,35 @@ def get_the_last_two_days_from(time_series: dict) -> (DailyStockData, DailyStock
 
 
 def calculate_difference(yesterday: DailyStockData, the_day_before_yesterday: DailyStockData):
-    return abs(yesterday.close - the_day_before_yesterday.close) / the_day_before_yesterday.close
+    return (yesterday.close - the_day_before_yesterday.close) / the_day_before_yesterday.close
 
 
-response = requests.get(STOCK_URL, params=params)
+response = requests.get(STOCK_URL, params=STOCK_PARAMS)
 time_series_dict = response.json().get("Time Series (Daily)")
 yesterday_data, the_day_before_yesterday_data = get_the_last_two_days_from(time_series_dict)
 
-if calculate_difference(yesterday=yesterday_data, the_day_before_yesterday=the_day_before_yesterday_data) > 0.05:
-    print("Get News")
+difference = calculate_difference(yesterday=yesterday_data, the_day_before_yesterday=the_day_before_yesterday_data)
+if abs(difference) > 0.001:
+    # STEP 2: Use https://newsapi.org
+    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+    response = requests.get(NEWS_URL, params=NEWS_PARAMS)
+    articles = response.json().get("articles")[:3]
 
-# TODO 2
-# STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+    # TODO 3
+    # STEP 3: Use https://www.twilio.com
+    # Send a separate message with the percentage change and each article's title and description to your phone number.
 
-# TODO 3
-# STEP 3: Use https://www.twilio.com
-# Send a separate message with the percentage change and each article's title and description to your phone number.
+    chosen_article = random.choice(articles)
 
+    body = f"""
+        TSLA:  {"🔺" if difference > 0 else "🔻"}{abs(difference)}%
+        Headline: {chosen_article.get("title")}. 
+        Brief: {chosen_article.get("description")}.
+        """
 
-# Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+    client = TwilioClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    message = client.messages.create(
+        body=body,
+        from_=TWILIO_FROM_NUMBER,
+        to=TWILIO_TO_NUMBER
+    )
